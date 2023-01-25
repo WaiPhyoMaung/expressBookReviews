6e27e73135cb55a754764f6011b64da71cce3db9
WaiPhyoMaung <waiphyomaung7@gmail.com>
2023-01-26 00:59:04 +0630

    Update with promise for general.js
    Signed-off-by: WaiPhyoMaung <waiphyomaung7@gmail.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,33 +25,100 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  res.send(books);
+  new Promise((resolve, reject) => {
+    res.send(JSON.stringify(books, null, 4));
+  })
+    .then((successMessage) => {
+      res.send(successMessage);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 // Get book details based on ISBN
+// public_users.get("/isbn/:isbn", function (req, res) {
+//   const isbn = req.params.isbn;
+//   res.send(books[isbn]);
+// });
+// public_users.get("/isbn/:isbn", function (req, res) {
+//   const isbn = req.params.isbn;
+//   new Promise((resolve, reject) => {
+//     res.send(books[isbn]);
+//   })
+//     .then((successMessage) => {
+//       res.send(successMessage);
+//     })
+//     .catch((error) => {
+//       res.send(error);
+//     });
+// });
+
 public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  let isbn = req.params.isbn;
+  new Promise((resolve, reject) => {
+    res.send(books[isbn]);
+  })
+    .then((successMessage) => {
+      res.send(successMessage);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 // Get book details based on author
+// public_users.get("/author/:author", function (req, res) {
+//   const author = req.params.author;
+//   const filteredBook = Object.values(books).filter((book) =>
+//     book.author.includes(author)
+//   );
+//   res.send(filteredBook);
+// });
+
 public_users.get("/author/:author", function (req, res) {
-  const author = req.params.author;
-  const filteredBook = Object.values(books).filter((book) =>
-    book.author.includes(author)
-  );
-  res.send(filteredBook);
+  let parameter = req.params.author;
+  new Promise((resolve, reject) => {
+    for (const key in books) {
+      if (books[key].author === parameter) {
+        res.send(`Book ${key} - Title: ${books[key].title}`);
+      }
+    }
+  })
+    .then((successMessage) => {
+      res.send(successMessage);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 // Get all books based on title
+// public_users.get("/title/:title", function (req, res) {
+//   const title = req.params.title;
+//   const filteredBook = Object.values(books).filter((book) =>
+//     book.title.includes(title)
+//   );
+//   res.send(filteredBook);
+// });
 public_users.get("/title/:title", function (req, res) {
-  const title = req.params.title;
-  const filteredBook = Object.values(books).filter((book) =>
-    book.title.includes(title)
-  );
-  res.send(filteredBook);
+  let parameter = req.params.title;
+  new Promise((resolve, reject) => {
+    for (const key in books) {
+      if (books[key].title === parameter) {
+        res.send(
+          `Book ${key} - Title: ${books[key].title} - Author: ${books[key].author}`
+        );
+      }
+    }
+  })
+    .then((successMessage) => {
+      res.send(successMessage);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
-
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
